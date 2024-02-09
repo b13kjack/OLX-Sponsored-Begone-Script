ec138e9e6601b8f22422d185da9a5d9bcad804b1
b13kjack <m.p.szczygiel@gmail.com>
2024-02-09 01:30:38 +0100

    Update olx-sponsored-begone.js

--- olx-sponsored-begone.js
+++ olx-sponsored-begone.js
@@ -1,3 +1,19 @@
+// ==UserScript==
+// @name         OLX Sponsored Begone
+// @namespace    https://github.com/b13kjack/OLX-Sponsored-Begone-Script
+// @version      2024-02-08
+// @description  Remove sponsored listings cus they annoy the shit out of me whenever I sort listings and try to browse them.
+// @author       You
+// @downloadURL  https://raw.githubusercontent.com/b13kjack/OLX-Sponsored-Begone-Script/main/olx-sponsored-begone.js
+// @updateURL    https://raw.githubusercontent.com/b13kjack/OLX-Sponsored-Begone-Script/main/olx-sponsored-begone.js
+// @supportURL   https://github.com/b13kjack/OLX-Sponsored-Begone-Script/issues
+// @match        https://olx.pl/*
+// @match        https://*.olx.pl/*
+// @match        https://olx.pl*
+// @match        https://*.olx.pl*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=olx.pl
+// @grant        none
+// ==/UserScript==
 function removeElementsWithText() {
     const parentElements = document.querySelectorAll('[data-cy="l-card"]');
 
