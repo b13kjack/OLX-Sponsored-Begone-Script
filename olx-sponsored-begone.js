dce76c3592270767e2682ca3c71681e9bd241c03
b13kjack <m.p.szczygiel@gmail.com>
2024-08-31 22:24:52 +0200

    Update olx-sponsored-begone.js

--- olx-sponsored-begone.js
+++ olx-sponsored-begone.js
@@ -8,8 +8,8 @@
 // @description:pl   Ukrywa oferty sponsorowane na olx.pl
 // @author           b13kjack
 // @license          MIT
-// @downloadURL      https://github.com/b13kjack/OLX-Sponsored-Begone-Script/raw/main/olx-sponsored-begone.js
-// @updateURL        https://github.com/b13kjack/OLX-Sponsored-Begone-Script/raw/main/olx-sponsored-begone.js
+// @downloadURL      https://github.com/b13kjack/OLX-Sponsored-Begone-Script/raw/main/olx-sponsored-begone.user.js
+// @updateURL        https://github.com/b13kjack/OLX-Sponsored-Begone-Script/raw/main/olx-sponsored-begone.user.js
 // @supportURL       https://github.com/b13kjack/OLX-Sponsored-Begone-Script/issues
 // @match            https://olx.pl/*
 // @match            https://*.olx.pl/*
